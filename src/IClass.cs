db1b3d8c20913aea1e58028ade6c5b32a24e6b07
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-05-16 00:18:42 +0200

    Create IClass.cs

---
+++ src/IClass.cs
@@ -0,0 +1,27 @@
+namespace playground
+{
+    public interface IClass
+    {
+        INamespace string Namespace { get; }
+        
+        IEnumerable<IUsing> Usings { get; }
+        
+        AccessModifier AccessModifier { get; }
+        
+        KeyIdentifier KeyIdentifier { get; }
+        
+        string BaseType { get; }
+        
+        IEnumerable<IInterface> Interfaces { get; }
+        
+        IEnumerable<IMember> Members { get; }
+        
+        IEnumerable<IProperty> Properties { get; }
+        
+        IConstructor Constructor { get; }
+        
+        IEnumerable<Func<T>> Functions { get; }
+        
+        IEnumerable<Action<T>> Actions { get; }
+    }
+}
